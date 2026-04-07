8f0f77a7da91aa20f457b916fa6208dadada76ac
Benjas333 <seston30@gmail.com>
2026-04-07 02:50:23 -0600

    vsconde-winbar, picker-lsp and signature_help Added heirline-vscode-winbar Added picker-lsp-mappings Added lsp.config.signature_help = true

--- lua/community.lua
+++ lua/community.lua
@@ -8,13 +8,20 @@
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
+
   -- import/override with your plugins folder
   { import = "astrocommunity.colorscheme.bluloco-nvim" },
+
+  { import = "astrocommunity.recipes.heirline-vscode-winbar" },
+  { import = "astrocommunity.recipes.picker-lsp-mappings" },
+
   -- { import = "astrocommunity.media.pets-nvim" },
   { import = "astrocommunity.media.cord-nvim" },
   { import = "astrocommunity.media.codesnap-nvim" },
+
   { import = "astrocommunity.workflow.hardtime-nvim" },
   { import = "astrocommunity.workflow.precognition-nvim" },
+
   { import = "astrocommunity.pack.rust" },
   { import = "astrocommunity.pack.python.base" },
   { import = "astrocommunity.pack.python.ruff" },
