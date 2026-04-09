ccbea1f6dc9bc8670d0a1725115578a0ae39276a
Benjas333 <seston30@gmail.com>
2026-04-09 14:10:43 -0600

    Bruh this were untracked the whole time LOL

---
+++ lua/plugins/codesnap-nvim.lua
@@ -0,0 +1,48 @@
+if true then return end
+
+local prefix = "<Leader>c"
+
+---@type LazySpec
+return {
+  "mistricky/codesnap.nvim",
+  tag = "v2.0.1",
+  -- build = "make",
+  cmd = {
+    "CodeSnap",
+    "CodeSnapSave",
+    "CodeSnapHighlight",
+    "CodeSnapSaveHighlight",
+  },
+  dependencies = {
+    {
+      "AstroNvim/astrocore",
+      opts = {
+        mappings = {
+          v = {
+            [prefix] = { desc = " CodeSnap" },
+            [prefix .. "s"] = { ":CodeSnap<CR>", desc = "CodeSnap (clipboard)" },
+            [prefix .. "S"] = { ":CodeSnapSave<CR>", desc = "CodeSnap (save)" },
+            [prefix .. "h"] = { ":CodeSnapHightlight<CR>", desc = "CondeSnap with highlight (clipboard)" },
+            [prefix .. "H"] = { ":CodeSnapSaveHighlight<CR>", desc = "CodeSnap with highlight (save)" },
+          },
+        },
+      },
+    },
+  },
+  opts = {
+    -- show_line_number = true,
+    snapshot_config = {
+      window = {
+        mac_window_bar = false,
+      },
+      code_config = {
+        breadcrumbs = {
+          -- enable = true,
+        },
+      },
+      watermark = {
+        content = "",
+      },
+    },
+  },
+}
---
+++ lua/plugins/nvim-strict.lua
@@ -0,0 +1,25 @@
+---@type LazySpec
+return {
+  -- "emileferreira/nvim-strict",
+  "Benjas333/nvim-strict",
+  branch = "fix-highlight",
+  -- commit = "cba2f989b66edd04181247f5cc25dcf36d556a80",
+  opts = function()
+    require("strict").setup {
+      deep_nesting = {
+        -- highlight_group = "Substitute",
+        depth_limit = 4,
+      },
+      overlong_lines = {
+        length_limit = 125,
+        split_on_save = false,
+      },
+      tab_indentation = {
+        convert_on_save = false,
+      },
+      todos = {
+        highlight = false,
+      },
+    }
+  end,
+}
