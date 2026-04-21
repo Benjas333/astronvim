4f4c32f9a434b78c4d10a3dfc6fd33e51e183cff
Benjas333 <seston30@gmail.com>
2026-04-20 19:01:22 -0600

    feat: changed neocord to cord.nvim, added nvim-training, updated formatting rules

---
+++ lua/plugins/cord-nvim.lua
@@ -0,0 +1,16 @@
+---@type LazySpec
+return {
+        "vyfor/cord.nvim",
+        opts = {
+                editor = { tooltip = "A better distro than LazyVim lol" },
+                buttons = {
+                        {
+                                label = function(opts) return opts.repo_url and "View Repository" or "View Project" end,
+                                url = function(opts) return opts.repo_url or "https://benjas333.github.io/Benjas333/" end,
+                        },
+                },
+                text = {
+                        workspace = function(opts) return "Working on " .. opts.workspace end,
+                },
+        },
+}
---
+++ lua/plugins/nvim-training.lua
@@ -0,0 +1,6 @@
+---@type LazySpec
+return {
+        "Weyaaron/nvim-training",
+        pin = true,
+        opts = {},
+}
