abdf2913f0790545b278808494dd9910c128ed6e
Benjas333 <seston30@gmail.com>
2026-04-20 19:10:33 -0600

    refactor: removed cord.nvim from community.lua to directly use cord-nvim.lua

--- lua/plugins/cord-nvim.lua
+++ lua/plugins/cord-nvim.lua
@@ -1,13 +1,18 @@
 ---@type LazySpec
 return {
   "vyfor/cord.nvim",
+  event = "VeryLazy",
   opts = {
-    editor = { tooltip = "A better distro than LazyVim lol" },
+    editor = {
+      client = "astronvim",
+      tooltip = "A better distro than LazyVim lol",
+    },
     buttons = {
       {
         label = function(opts) return opts.repo_url and "View Repository" or "View Project" end,
         url = function(opts) return opts.repo_url or "https://benjas333.github.io/Benjas333/" end,
       },
+      -- TODO: add View Webpage
     },
     text = {
       workspace = function(opts) return "Working on " .. opts.workspace end,
